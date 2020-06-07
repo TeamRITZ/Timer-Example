d3bd71e8d3b9037cc5c769f46bb4cf0baabe0e4c
Bluestopsign98 <jamesmele01@hotmail.com>
2020-06-06 21:05:16 -0400

    fixed program

--- TimerExample.gd
+++ TimerExample.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
 # warning-ignore:return_value_discarded
-	$Timer.connect("timeout",self,_on_Timer_timeout())
+	$Timer.connect("timeout",self,"_on_Timer_timeout")
 
 func _on_Timer_timeout():
 	$Sprite.visible = !$Sprite.visible
